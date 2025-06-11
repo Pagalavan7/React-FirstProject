103309551d3a6ef48fbfc6afb4329200995bddb0
Pagalavan7 <pagalavanvetri@gmail.com>
2025-06-11 09:23:10 +0530

    basic app setup learn

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import ProductComponent from "./Components/Products";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export function App() {
+  let message = "Awesome";
+
+  // return <h2>This is App Component {message} </h2>;
+  //cannot return 2 html elements directly.. //to do that we have to wrap that with a container element
+
+  // return (
+  //   <div>
+  //     <h1 className="header">
+  //       I am learning React {message} {2 + 3}
+  //     </h1>
+  //     <p className="para">Learning CSS XML syntax and others</p>
+  //   </div>
+  // );
+
+  // let p = document.createElement("p");
+  // p.innerHTML = "Hey im creating app using js";
+  // return p;
+
+  return <ProductComponent />;
 }
-
-export default App;
---
+++ src/Components/Products.js
@@ -0,0 +1,37 @@
+function ProductComponent() {
+  return (
+    <ul>
+      <li>
+        <div>
+          <div>
+            <h5>Fresh Milk</h5>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit
+              fuga autem maiores necessitatibus.
+            </p>
+            <div>
+              <h6>$120.00</h6>
+            </div>
+          </div>
+          <img src="" alt="" width="200"></img>
+        </div>
+      </li>
+      <li>
+        <div>
+          <div>
+            <h5>Fresh Milk</h5>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit
+              fuga autem maiores necessitatibus.
+            </p>
+            <div>
+              <h6>$120.00</h6>
+            </div>
+          </div>
+          <img src="" alt="" width="200"></img>
+        </div>
+      </li>
+    </ul>
+  );
+}
+export default ProductComponent;
